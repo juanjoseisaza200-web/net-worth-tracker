799f0298bd150dc75dd820bde181bf8caec81d8f
Juan Jose Isaza <juanjoseisaza200@icloud.com>
2026-02-13 23:17:17 -0500

    feat(ui): Float bottom navigation bar

--- src/App.tsx
+++ src/App.tsx
@@ -45,7 +45,7 @@ function Navigation() {
   ];
 
   return (
-    <nav className="fixed bottom-0 left-0 right-0 bg-white border-t border-gray-200 shadow-lg">
+    <nav className="fixed bottom-4 left-4 right-4 bg-white border border-gray-200 shadow-lg rounded-2xl">
       <div className="flex justify-around items-center h-16">
         {navItems.map((item) => {
           const Icon = item.icon;
